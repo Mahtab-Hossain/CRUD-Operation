e041fb80a7d534df303e0e2f92ae7e5ad2edacb9
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-26 02:24:58 +0600

    modified index file and added custom css file link

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <!-- bootstrap css file only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+        <!-- custom css file -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
